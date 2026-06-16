9a180d3b46812c09c589568a33168121a5189b58
qHxgre <hxgre947173@gmail.com>
2026-06-16 09:56:44 +0800

    因子进行预处理

--- data/bigalpha_2026_factorlib/builder.py
+++ data/bigalpha_2026_factorlib/builder.py
@@ -38,7 +38,42 @@ class Bigalpha2026FactorlibBuilder(BaseBuilder):
 
     def get_data(self, start_date: str, end_date: str) -> pd.DataFrame:
         before_start_date = (datetime.strptime(self.start_date, '%Y-%m-%d') - timedelta(days=365)).strftime('%Y-%m-%d')
-        sql = """
+
+        def _build_normalize_sql(col: str, group_by: str = "date") -> str:
+            avg_expr  = f"c_avg(i.{col}, pb:={group_by})"
+            std_expr  = f"c_std(i.{col}, pb:={group_by})"
+            clip_expr = (
+                f"clip(i.{col}, "
+                f"{avg_expr} - 3*{std_expr}, "
+                f"{avg_expr} + 3*{std_expr}"   # 修复：原来这里重新写了 c_std，与上面不一致
+                f") as _{col}"
+            )
+            norm_expr = f"c_normalize(_{col}, pd:={group_by}) as {col}"
+            return clip_expr + ",\n" + norm_expr
+
+        NORMALIZE_COLS = [
+            # 行情基础
+            "close", "volume", "amount", "turn", "change_ratio", "daily_return",
+            # 动量/波动
+            "momentum_5", "reversal_5", "volatility_5",
+            # 市值/估值
+            "total_market_cap", "float_market_cap", "pe_ttm", "pb", "ps_ttm",
+            # 均线/MACD
+            "sma_20", "ema_20",
+            "macd_diff_12_26_9", "macd_dea_12_26_9", "macd_hist_12_26_9", "rsi_12",
+            # 其他技术指标
+            "kdj_k_9_3_3", "kdj_d_9_3_3", "bias_20", "cci_14", "atr_14",
+            # 基本面
+            "roe_avg_ttm", "roa_avg_ttm", "gross_profit_rate_ttm", "net_profit_rate_ttm",
+            "debt_to_asset_lf", "current_ratio_lf",
+            # 资金流/其他
+            "netflow_amount_main", "netflow_amount_rate_main",
+            "net_active_buy_amount_main", "beta_000300SH_22", "list_days",
+        ]
+
+        # 修复：用 join 收集所有列，而不是反复覆盖 sql 变量
+        factor_sql = ",\n".join(_build_normalize_sql(col) for col in NORMALIZE_COLS)
+        sql = f"""
         WITH cte_index AS (
             SELECT date, member_code AS instrument
             FROM cn_stock_index_component
@@ -46,42 +81,7 @@ class Bigalpha2026FactorlibBuilder(BaseBuilder):
         )
         SELECT 
             i.date, i.instrument,
-            i.close,
-            i.volume,
-            i.amount,
-            i.turn,
-            i.change_ratio,
-            i.daily_return,
-            i.momentum_5,
-            i.reversal_5,
-            i.volatility_5,
-            i.total_market_cap,
-            i.float_market_cap,
-            i.pe_ttm,
-            i.pb,
-            i.ps_ttm,
-            i.sma_20,
-            i.ema_20,
-            i.macd_diff_12_26_9,
-            i.macd_dea_12_26_9,
-            i.macd_hist_12_26_9,
-            i.rsi_12,
-            i.kdj_k_9_3_3,
-            i.kdj_d_9_3_3,
-            i.bias_20,
-            i.cci_14,
-            i.atr_14,
-            i.roe_avg_ttm,
-            i.roa_avg_ttm,
-            i.gross_profit_rate_ttm,
-            i.net_profit_rate_ttm,
-            i.debt_to_asset_lf,
-            i.current_ratio_lf,
-            i.netflow_amount_main,
-            i.netflow_amount_rate_main,
-            i.net_active_buy_amount_main,
-            i.beta_000300SH_22,
-            i.list_days,
+            {factor_sql}
         FROM cn_stock_prefactors i
         INNER JOIN cte_index c
             ON i.date = c.date
