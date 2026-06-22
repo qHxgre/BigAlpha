f63cc41bd6ce0a908b40150957f3f71882f852b4
qHxgre <hxgre947173@gmail.com>
2026-06-22 17:57:17 +0800

    增加分钟的测试集、私榜数据

---
+++ data/bigalpha_2026_stock_bar1m_private/schema.py
@@ -0,0 +1,59 @@
+import numpy as np
+import pandas as pd
+from pydantic import Field
+
+from base import BaseSchema
+
+
+class Bigalpha2026StockBar1mPrivateSchema(BaseSchema):
+    date: np.datetime64 = Field(description="日期", default=0)
+    instrument: pd.StringDtype = Field(description="股票代码", default=np.nan)
+
+    adjust_factor: np.float32 = Field(description="累计后复权因子", default=np.nan)
+    pre_close: np.float32 = Field(description="前收盘价", default=np.nan)
+    high: np.float32 = Field(description="最高价", default=np.nan)
+    open: np.float32 = Field(description="开盘价", default=np.nan)
+    low: np.float32 = Field(description="最低价", default=np.nan)
+    close: np.float32 = Field(description="收盘价", default=np.nan)
+    deal_number: np.int32 = Field(description="成交笔数", default=0)
+    volume: np.int32 = Field(description="最新总成交量", default=0)
+    amount: np.float32 = Field(description="最新成交金额", default=np.nan)
+
+    # 委托价格
+    ask_price1: np.float32 = Field(description="1档委卖价", default=0)
+    ask_price2: np.float32 = Field(description="2档委卖价", default=0)
+    ask_price3: np.float32 = Field(description="3档委卖价", default=0)
+    ask_price4: np.float32 = Field(description="4档委卖价", default=0)
+    ask_price5: np.float32 = Field(description="5档委卖价", default=0)
+    bid_price1: np.float32 = Field(description="1档委买价", default=0)
+    bid_price2: np.float32 = Field(description="2档委买价", default=0)
+    bid_price3: np.float32 = Field(description="3档委买价", default=0)
+    bid_price4: np.float32 = Field(description="4档委买价", default=0)
+    bid_price5: np.float32 = Field(description="5档委买价", default=0)
+
+    # 委托量
+    ask_volume1: np.int32 = Field(description="1档委卖量", default=0)
+    ask_volume2: np.int32 = Field(description="2档委卖量", default=0)
+    ask_volume3: np.int32 = Field(description="3档委卖量", default=0)
+    ask_volume4: np.int32 = Field(description="4档委卖量", default=0)
+    ask_volume5: np.int32 = Field(description="5档委卖量", default=0)
+    bid_volume1: np.int32 = Field(description="1档委买量", default=0)
+    bid_volume2: np.int32 = Field(description="2档委买量", default=0)
+    bid_volume3: np.int32 = Field(description="3档委买量", default=0)
+    bid_volume4: np.int32 = Field(description="4档委买量", default=0)
+    bid_volume5: np.int32 = Field(description="5档委买量", default=0)
+
+    # 委托比数
+    ask_num_orders1: np.int32 = Field(description="卖1档委托笔数", default=0)
+    ask_num_orders2: np.int32 = Field(description="卖2档委托笔数", default=0)
+    ask_num_orders3: np.int32 = Field(description="卖3档委托笔数", default=0)
+    ask_num_orders4: np.int32 = Field(description="卖4档委托笔数", default=0)
+    ask_num_orders5: np.int32 = Field(description="卖5档委托笔数", default=0)
+    bid_num_orders1: np.int32 = Field(description="买1档委托笔数", default=0)
+    bid_num_orders2: np.int32 = Field(description="买2档委托笔数", default=0)
+    bid_num_orders3: np.int32 = Field(description="买3档委托笔数", default=0)
+    bid_num_orders4: np.int32 = Field(description="买4档委托笔数", default=0)
+    bid_num_orders5: np.int32 = Field(description="买5档委托笔数", default=0)
+
+    class Config:
+        arbitrary_types_allowed = True
---
+++ data/bigalpha_2026_stock_bar1m_test/builder.py
@@ -0,0 +1,67 @@
+import dai
+import pandas as pd
+from datetime import datetime
+
+from base import BaseBuilder
+from bigalpha_2026_stock_bar1m_test.schema import Bigalpha2026StockBar1mTestSchema
+
+
+class Bigalpha2026StockBar1mTestBuilder(BaseBuilder):
+    datasource_id = "bigalpha_2026_stock_bar1m_test"
+    unique_together = ["date", "instrument"]
+    sort_by = [("date", "ascending"), ("instrument", "ascending")]
+    indexes = ["date"]
+    schema = Bigalpha2026StockBar1mTestSchema
+
+
+    def __init__(self, start_date: str, end_date: str) -> None:
+        self.start_date = start_date
+        self.end_date = end_date
+        print(f"初始化！{self.datasource_id}, 时间周期: {self.start_date}, {self.end_date}")
+
+    def normalize(self, df: pd.DataFrame) -> pd.DataFrame:
+        df = df.reindex(columns=self.schema.columns())
+        df = df.astype(self.schema.field_type_mapping())
+        df = df.fillna(self.schema.field_default_mapping())
+        return df
+
+    def dai_write(self, df: pd.DataFrame):
+        default_docs = self.schema.default_docs()
+        df[dai.DEFAULT_PARTITION_FIELD] = df["date"].dt.strftime("%Y%m").astype("int64")
+        dai.DataSource.write_bdb(
+            df,
+            id=self.datasource_id,
+            unique_together=self.unique_together,
+            sort_by=self.sort_by,
+            indexes=self.indexes,
+            docs=default_docs,
+        )
+    def get_data(self, start_date: str, end_date: str) -> pd.DataFrame:
+        sql = """
+        WITH cte_index AS (
+            SELECT CAST(strftime(date, '%Y%m%d') AS INT32) AS trading_day, member_code as instrument
+            FROM cn_stock_index_component
+            WHERE instrument = '000852.SH'
+        )
+        SELECT s.*
+        FROM cn_stock_bar1m_derived_c s
+        INNER JOIN cte_index
+            ON s.trading_day = cte_index.trading_day
+            AND s.instrument = cte_index.instrument
+        """
+
+        df = dai.query(sql, filters={"date": [f"{start_date} 00:00:00", f"{end_date} 23:59:59"]}, compression=True).df()
+        return df
+
+    def build(self) -> pd.DataFrame:
+        # 读取数据
+        t0 = datetime.now()
+        df = self.get_data(self.start_date, self.end_date)
+        t1 = datetime.now()
+        print(f"获取数据耗时: {round((t1-t0).total_seconds(), 4)} 秒")
+
+        # 存储数据
+        df = self.normalize(df)
+        self.dai_write(df)
+        t2 = datetime.now()
+        print(f"数据存储耗时: {round((t2-t1).total_seconds(), 4)} 秒")
